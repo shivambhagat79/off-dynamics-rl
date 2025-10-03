a809a48bcf384015f3a0906ca585a2063ed9b46d
shivambhagat79 <shivambhagat2279+github@gmail.com>
2025-10-03 11:27:31 +0000

    nomad_weight ad runs

--- algo/call_algo.py
+++ algo/call_algo.py
@@ -9,7 +9,7 @@ def call_algo(algo_name, config, mode, device):
         policy = SAC_Target_Only(config, device)
     elif mode == 0:
         algo_name = algo_name.lower()
-        assert algo_name in ['sac', 'darc', 'vgdf', 'sac_iw', 'par', 'sac_liberty', 'darc_liberty', 'larc', 'epic', 'nomad', 'nomad_v2', 'nomad_v3']
+        assert algo_name in ['sac', 'darc', 'vgdf', 'sac_iw', 'par', 'sac_liberty', 'darc_liberty', 'larc', 'epic', 'nomad', 'nomad_v2', 'nomad_v3', 'nomad_weight']
         # online online setting
         from online_online.darc import DARC
         from online_online.sac import SAC
@@ -23,6 +23,7 @@ def call_algo(algo_name, config, mode, device):
         from online_online.nomad import NOMAD
         from online_online.nomad_v2 import NOMAD_V2
         from online_online.nomad_v3 import NOMAD_V3
+        from online_online.nomad_weight import NOMAD_V3 as NOMAD_WEIGHT
 
         algo_to_call = {
             'sac': SAC,
@@ -37,6 +38,7 @@ def call_algo(algo_name, config, mode, device):
             'nomad': NOMAD,
             'nomad_v2': NOMAD_V2,
             'nomad_v3': NOMAD_V3,
+            'nomad_weight': NOMAD_WEIGHT,
         }
 
         algo = algo_to_call[algo_name]
